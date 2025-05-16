76d4e72aca9245a0de923e818daf2eaff36a9cf4
XianDe-Zeng <156569749+Zzeng0917@users.noreply.github.com>
2025-05-16 18:33:39 +0800

    Create consistenthash.go

---
+++ day2-single-node/geecache/consistenthash/consistenthash.go
@@ -0,0 +1,52 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type Hash func(data []byte) uint32
+
+type Map struct {
+	hash     Hash
+	replicas int            //虚拟节点倍数
+	keys     []int          //哈希环
+	hashMap  map[int]string //虚拟节点与真实节点的映射
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			//通过添加编号的方式区分不同的虚拟节点
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+	hash := int(m.hash([]byte(key)))
+	//顺时针找到第一个匹配的虚拟节点的下标
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
