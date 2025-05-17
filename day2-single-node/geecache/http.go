7073cbbfc975b33cf84b06f14bd83491f2f7e3e9
XianDe-Zeng <156569749+Zzeng0917@users.noreply.github.com>
2025-05-17 22:30:30 +0800

    Update http.go

--- day2-single-node/geecache/http.go
+++ day2-single-node/geecache/http.go
@@ -1,21 +1,35 @@
 package geecache
 
 import (
+	"Geecache/day2-single-node/geecache/consistenthash"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_geecache"
+const (
+	defaultBasePath = "/_geecache"
+	defaultReplicas = 50
+)
 
 type HTTPPool struct {
-	self     string //记录自己的地址，包括主机名/Ip和端口
-	basePath string //节点间通讯地址的前缀，默认/_geecache/
+	self        string //记录自己的地址，包括主机名/Ip和端口
+	basePath    string //节点间通讯地址的前缀，默认/_geecache/
+	mu          sync.RWMutex
+	peers       *consistenthash.Map
+	httpGetters map[string]*httpGetter
+}
+
+type httpGetter struct {
+	baseURL string
 }
 
 func (p *HTTPPool) Handler() http.Handler {
-	return http.HandlerFunc(p.SeverHTTP)
+	return http.HandlerFunc(p.ServeHTTP)
 }
 func GetGroup(groupName string) *Group {
 	return groups[groupName]
@@ -31,7 +45,8 @@ func (p *HTTPPool) Log(format string, v ...interface{}) {
 	log.Printf("[Sever %s] %s", p.self, fmt.Sprintf(format, v...))
 }
 
-func (p *HTTPPool) SeverHTTP(w http.ResponseWriter, r *http.Request) {
+// 从 SeverHTTP 改为 ServeHTTP
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if !strings.HasPrefix(r.URL.Path, p.basePath) {
 		panic("HTTPPool serving unexpected path:" + r.URL.Path)
 	}
@@ -56,6 +71,55 @@ func (p *HTTPPool) SeverHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("content-type", "application/octet-scream")
+	w.Header().Set("content-type", "application/octet-stream")
 	w.Write(view.b)
 }
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("sever reutrned: %v", res.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return bytes, nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.RLock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
