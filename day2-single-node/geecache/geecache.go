4d8ff90c12b29c911fa3cb2393d08ac9a8bf7943
XianDe-Zeng <156569749+Zzeng0917@users.noreply.github.com>
2025-05-18 21:39:38 +0800

    Update geecache.go

--- day2-single-node/geecache/geecache.go
+++ day2-single-node/geecache/geecache.go
@@ -1,6 +1,7 @@
 package geecache
 
 import (
+	"Geecache/day2-single-node/geecache/singleflight"
 	"fmt"
 	"log"
 	"reflect"
@@ -18,6 +19,8 @@ type Group struct {
 	name      string
 	getter    Getter
 	mainCache cache
+	peers     PeerPicker
+	loader    *singleflight.Group
 }
 
 var (
@@ -50,8 +53,8 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
-	groups[name] = g
 	return g
 }
 
@@ -73,10 +76,6 @@ func (g *Group) Get(key string) (ByteView, error) {
 	return g.load(key)
 }
 
-func (g *Group) load(key string) (value ByteView, err error) {
-	return g.getLocally(key)
-}
-
 func (g *Group) getLocally(key string) (ByteView, error) {
 	bytes, err := g.getter.Get(key)
 	if err != nil {
@@ -90,3 +89,48 @@ func (g *Group) getLocally(key string) (ByteView, error) {
 func (g *Group) populateCache(key string, value ByteView) {
 	g.mainCache.add(key, value)
 }
+
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+} //将PeerPicker接口的HTTPPool注入到Group中
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok {
+			if value, err = g.getFromPeer(peer, key); err == nil {
+				return value, nil
+			}
+			log.Println("[GeeCache] failed to get from peer", err)
+		}
+	}
+	return g.getLocally(key)
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
+} //实现PeerGetter接口的httpGetter访问远程节点，获取缓存值
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] failed to get from peer", err)
+			}
+		}
+		return g.getLocally(key)
+	})
+	if err != nil {
+		return viewi.ByteView(), nil
+	}
+	return
+} //使用singleflight实现缓存加载
